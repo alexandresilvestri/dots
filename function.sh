98785cacb2aa9b839f4841f9a0917fb1b33ba0ab
gaspar <airtontunni@gmail.com>
2025-12-06 17:03:28 -0300

    add new aliases to fish config

--- function.sh
+++ function.sh
@@ -1,11 +1,3 @@
-function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s@%s %s%s%s > ' $USER $hostname \
-        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-end
-
 set -eg QT_QPA_PLATFORM 2>/dev/null
 set -gx QT_QPA_PLATFORM xcb
 
@@ -20,22 +12,24 @@ if status is-interactive # Commands to run in interactive sessions can go here
     set fish_greeting
 
     # Use starship
-    starship init fish | source
+    # starship init fish | source
     if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
         cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
     end
 
     # Aliases
-    alias pamcan pacman
     alias ls 'eza --icons'
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias q 'qs -c ii'
-    
+    alias gc 'git commit -m'
+    alias dcu 'docker compose up -d'
+    alias dcd 'docker compose down -v'
+    alias arch "sudo pacman"
+    alias update "arch -Syu"
+    alias cat "bat --paging=never"
+
 end
 
-# my alias
-abbr -a cat "bat --paging=never"
-alias arch "sudo pacman"
-alias update "arch -Syu"
-
 zoxide init fish | source
+
+
