ff7cc4f536390fde82f9b9a4e780718048f2797d
Alexandre Silvestri <airtontunni@gmail.com>
2026-05-22 13:25:38 -0300

    remove unnecessary camera light booster script

--- scripts/.camera.sh
+++
@@ -1,7 +0,0 @@
-	camera-setup () {
-        deviceName="USB Camera"
-        device=$(v4l2-ctl --list-devices | grep -A 1 $deviceName | tail  -n 1 | sed 's/\t//')
-        bright=${1:-255}
-        echo "Setting bright in $device to $bright..."
-        sudo v4l2-ctl -d $device -c led1_mode=3 -c brightness=$bright
-	}
