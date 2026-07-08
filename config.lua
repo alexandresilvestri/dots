fd59f2288015fe6ad0df4f46ad73f0f75f0da191
Alexandre Silvestri <alexandre.silvestri@jc2sec.com.br>
2026-07-08 08:50:53 -0300

    lunarvim config

---
+++ config.lua
@@ -0,0 +1,10 @@
+lvim.builtin.nvimtree.setup.view.width = { min = 20, max = 30 }
+lvim.builtin.nvimtree.setup.view.adaptive_size = true
+
+lvim.builtin.indentlines.options.use_treesitter = false
+lvim.builtin.indentlines.options.use_treesitter_scope = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
