a47b9cdeae12a1216c93dc66c7aebd195d94241e
Alexandre Silvestri <alexandre.silvestri@jc2sec.com.br>
2026-07-08 09:37:11 -0300

    default two spaces for python ruff check on config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,10 @@ lvim.builtin.nvimtree.setup.view.adaptive_size = true
 lvim.builtin.indentlines.options.use_treesitter = false
 lvim.builtin.indentlines.options.use_treesitter_scope = false
 
+require("lvim.lsp.manager").setup("ruff", {
+  cmd = { "ruff", "server" },
+})
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
