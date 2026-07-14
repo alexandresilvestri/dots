03722e6eb08d99476ea3fad32aba5d48dbb13966
Alexandre Silvestri <alexandre.silvestri@jc2sec.com.br>
2026-07-14 11:37:28 -0300

    add inline git blame to lunarvim config

--- config.lua
+++ config.lua
@@ -12,3 +12,12 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+
+-- Inline Gitblame
+lvim.builtin.gitsigns.opts.current_line_blame = true
+lvim.builtin.gitsigns.opts.current_line_blame_opts = {
+  delay = 300,
+  virt_text_pos = "eol", -- end of line
+}
+lvim.builtin.gitsigns.opts.current_line_blame_formatter =
+  "  <author>, <author_time:%Y-%m-%d> · <summary>"
