200874eba3f3926dfa4916dfa8a6d1eb37c04562
Alexandre Silvestri <airtontunni@gmail.com>
2026-08-25 16:37:11 -0300

    adapt personal omarchy config to quattro update

--- omarchy/hypr/bindings.lua
+++ omarchy/hypr/bindings.lua
@@ -6,3 +6,4 @@ o.bind("SUPER + T", "Tmux", o.launch([[xdg-terminal-exec --dir="$(omarchy-cmd-te
 o.bind("SUPER + S", "Music", o.launch_sole("spotify", "flatpak run com.spotify.Client"))
 o.bind("SUPER + E", "File manager", o.launch("nautilus --new-window"))
 o.bind("SUPER + F", "Firefox", o.launch("firefox"))
+o.bind("ALT + F4", "Close window", hl.dsp.window.close())
